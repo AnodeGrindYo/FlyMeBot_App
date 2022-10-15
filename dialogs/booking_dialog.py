72c1c82e037c4839f08499e5ac7a1be1bad595fa
adriengodoy <mr.a.godoy@gmail.com>
2022-10-15 16:58:56 +0200

    More log added to final_step to investigate why the confirm prompt is inverted when deployed

--- dialogs/booking_dialog.py
+++ dialogs/booking_dialog.py
@@ -211,7 +211,11 @@ class BookingDialog(CancelAndHelpDialog):
     async def final_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         """Complete the interaction and end the dialog."""
         booking_details = step_context.options
-        
+        self.logger.info(step_context.result)
+        import sys
+        print(step_context.options, file=sys.stdout)
+        print(step_context.result, file=sys.stdout)
+        print(step_context.values, file=sys.stdout)
         if step_context.result:
             self.logger.setLevel(logging.INFO)
             self.logger.info('Flight booked, customer satisfied')
